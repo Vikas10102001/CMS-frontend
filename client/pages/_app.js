171477f8dc0f27feed3854ff46bbf7c80fe09d5a
Vikas <provikas2001@gmail.com>
2023-05-02 21:24:56 +0530

    added pages for routing

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -1,5 +1,15 @@
-import "antd/dist/reset.css"
+import { ConfigProvider, theme } from "antd";
+import NavBar from "../components/NavBar";
 
 export default function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <ConfigProvider
+      theme={{
+        algorithm: theme.defaultAlgorithm,
+      }}
+    >
+      <NavBar />
+      <Component {...pageProps} />
+    </ConfigProvider>
+  );
 }
---
+++ client/pages/admin/index.js
@@ -0,0 +1,11 @@
+import { Button } from 'antd'
+import React from 'react'
+
+export default function index() {
+  return (
+    <>
+    <h1>Admin</h1>
+    <Button type='primary'>Click</Button>
+    </>
+  )
+}
