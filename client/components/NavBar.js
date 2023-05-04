9828db63c9a33d172022759555053a97298da559
Vikas <provikas2001@gmail.com>
2023-05-04 22:10:42 +0530

    toggle theme using button in setting

--- client/components/NavBar.js
+++ client/components/NavBar.js
@@ -1,5 +1,6 @@
 import {
   AppstoreOutlined,
+  BulbOutlined,
   LogoutOutlined,
   RocketOutlined,
   SettingOutlined,
@@ -7,61 +8,15 @@ import {
 import { Menu } from "antd";
 import { useEffect, useState } from "react";
 import Link from "next/link";
-const items = [
-  {
-    label: <Link href="/">CMS</Link>,
-    key: "logo",
-    style: { fontWeight: "bold", fontSize: "1.8em" },
-  },
-  {
-    label: <Link href="/signin">Signin</Link>,
-    key: "signin",
-    icon: <AppstoreOutlined />,
-  },
-  {
-    label: <Link href="/signup">Signup</Link>,
-    key: "signup",
-    icon: <AppstoreOutlined />,
-  },
-  {
-    label: "Dashboard",
-    key: "dashboard",
-    style: { marginLeft: "auto" },
-    children: [
-      {
-        type: "group",
-        label: "Management",
-        children: [{ label: <Link href="/admin">Admin</Link>, key: "admin" }],
-      },
-    ],
-  },
-  {
-    label: "Settings",
-    key: "settings",
-    icon: (
-      <SettingOutlined
-        style={{
-          fontSize: "1.2em",
-        }}
-      />
-    ),
-    children: [
-      {
-        label: `Mode : Dark`,
-        key: "mode",
-        icon: <RocketOutlined />,
-      },
-      {
-        label: "Logout",
-        key: "logout",
-        icon: <LogoutOutlined />,
-      },
-    ],
-  },
-];
+import store from "../store/store";
+import themeSlice from "../store/slice/theme";
+import { useSelector } from "react-redux";
 
 const NavBar = () => {
   const [current, setCurrent] = useState("mail");
+  const theme = useSelector((state) => {
+    return state.theme.dark;
+  });
   useEffect(() => {
     if (!localStorage.getItem("currentPathKey")) {
       localStorage.setItem("currentPathKey", "logo");
@@ -71,12 +26,66 @@ const NavBar = () => {
   }, []);
   const onClick = (e) => {
     if (e.key !== "mode" && e.key !== "logout") {
-      console.log("here");
       localStorage.setItem("currentPathKey", e.key);
       setCurrent(e.key);
     }
   };
-  console.log(current);
+  const handleChangeMode = () => {
+    store.dispatch(themeSlice.actions.setTheme(!theme));
+  };
+  const items = [
+    {
+      label: <Link href="/">CMS</Link>,
+      key: "logo",
+      style: { fontWeight: "bold", fontSize: "1.8em" },
+    },
+    {
+      label: <Link href="/signin">Signin</Link>,
+      key: "signin",
+      icon: <AppstoreOutlined />,
+    },
+    {
+      label: <Link href="/signup">Signup</Link>,
+      key: "signup",
+      icon: <AppstoreOutlined />,
+    },
+    {
+      label: "Dashboard",
+      key: "dashboard",
+      style: { marginLeft: "auto" },
+      children: [
+        {
+          type: "group",
+          label: "Management",
+          children: [{ label: <Link href="/admin">Admin</Link>, key: "admin" }],
+        },
+      ],
+    },
+    {
+      label: "Settings",
+      key: "settings",
+      icon: (
+        <SettingOutlined
+          style={{
+            fontSize: "1.2em",
+          }}
+        />
+      ),
+      children: [
+        {
+          label: `Mode : ${theme ? "Dark" : "light"}`,
+          key: "mode",
+          icon: theme ? <RocketOutlined /> : <BulbOutlined />,
+          onClick: handleChangeMode,
+        },
+        {
+          label: "Logout",
+          key: "logout",
+          icon: <LogoutOutlined />,
+        },
+      ],
+    },
+  ];
   return (
     <Menu
       onClick={onClick}
