f10470e1e1b02df9f4b164414d744a2b8db235b6
Vikas <provikas2001@gmail.com>
2023-05-04 20:35:14 +0530

    setting the theme according to the current theme state

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -1,15 +1,30 @@
 import { ConfigProvider, theme } from "antd";
 import NavBar from "../components/NavBar";
+import store from "../store/store";
+import { Provider } from "react-redux";
+import { useEffect, useState } from "react";
 
 export default function MyApp({ Component, pageProps }) {
+  const [darkTheme, setDarkTheme] = useState(store.getState().theme.dark);
+
+  useEffect(() => {
+    const unsubscribe = store.subscribe(() => {
+      setDarkTheme(store.getState().theme.dark);
+    });
+    return () => {
+      unsubscribe();
+    };
+  }, []);
   return (
-    <ConfigProvider
-      theme={{
-        algorithm: theme.defaultAlgorithm,
-      }}
-    >
-      <NavBar />
-      <Component {...pageProps} />
-    </ConfigProvider>
+    <Provider store={store}>
+      <ConfigProvider
+        theme={{
+          algorithm: darkTheme ? theme.darkAlgorithm : theme.defaultAlgorithm,
+        }}
+      >
+        <NavBar />
+        <Component {...pageProps} />
+      </ConfigProvider>
+    </Provider>
   );
 }
